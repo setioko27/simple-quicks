eb6eb4fc900b6638daa459e7d207e4f7659c9410
Tio <tio@harnods.com>
2023-04-02 16:28:29 +0700

    Call toast style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Input } from 'antd';
 import {SearchOutlined} from "@ant-design/icons"
 
 import "antd/dist/reset.css";
+import "react-toastify/dist/ReactToastify.css";
 import "./styles/app.sass"
 import Actions from 'components/Actions';
 import { ToastContainer } from 'react-toastify';
